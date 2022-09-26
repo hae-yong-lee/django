c81c17c07431debe3630db746c5c7d147760e98c
이해용 <hyunggok@gumi.ac.kr>
2022-09-26 18:17:38 +0900

    연습-02 장고 7/14 파트 연습

--- gumi/myapp/views.py
+++ gumi/myapp/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, HttpResponse
-import random
 
+topics = [
+    {'id':1, 'title':'routing', 'body':'Routing is ..'},
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'model', 'body':'Model is ..'},
+]
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>랜덤 자료</h1>'+str(random.random()))
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return HttpResponse(f'''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        <h2>Welcom</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''')
 
 def create(request):
     return HttpResponse('안녕하세요. Create 페이지 입니다.')
